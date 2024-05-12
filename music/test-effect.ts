b039984f19a4ff97470d95e9d7418244784ae7cc
hiett <sodex.gaming@gmail.com>
2024-05-12 23:19:15 +0100

    yes

--- music/test-effect.ts
+++ music/test-effect.ts
@@ -1,13 +1,15 @@
 import {MiniKintaILS} from '@dmxjs/fixtures';
-import {type MusicContext} from './src';
+import {Energy, type MusicContext} from './src';
 import {Color} from '@dmxjs/shared';
 
 export class MiniKintaILSDownBeatFlash extends MiniKintaILS<MusicContext> {
 	override render(ctx: MusicContext): Buffer {
 		const frame = this.createFrame();
 
-		const color = Color.random();
+		const color = new Color(0, 255, 0);
 		this.setColor(frame, color);
+
+		console.log(Energy[ctx.energy]);
 
 		// Create a sine wave based on the beat in the song as the speed
 		// \frac{\left(\sin\left(\frac{x}{5-\left(z-1\right)}\right)+1\right)}{2}
